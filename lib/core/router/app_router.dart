13f92093af9f7dc446343ad8ff364b53da94404a
rzaldii <iqbal17.rizaldi@gmail.com>
2026-06-07 22:25:02 +0700

    test

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -4,12 +4,12 @@ import '../../features/admin/dashboard/admin_dashboard_screen.dart';
 import '../../features/auth/presentation/login_screen.dart';
 import '../../features/customer/home/customer_home_screen.dart';
 import '../../features/customer/items/presentation/item_detail_screen.dart';
-import '../../features/customer/verification/presentation/identity_verification_screen.dart';
 import '../../features/splash/splash_screen.dart';
 import '../../features/customer/booking/presentation/create_booking_screen.dart';
 import '../../features/customer/booking/presentation/booking_detail_screen.dart';
 import '../../features/customer/booking/presentation/customer_bookings_screen.dart';
 import '../../features/customer/booking/presentation/payment_webview_screen.dart';
+import '../../features/customer/verification/presentation/identity_verification_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
@@ -36,6 +36,55 @@ final appRouter = GoRouter(
         final id = int.tryParse(state.pathParameters['id'] ?? '') ?? 0;
 
         return ItemDetailScreen(itemId: id);
+      },
+    ),
+    GoRoute(
+      path: '/customer/booking/create/:itemId',
+      name: 'customer-create-booking',
+      builder: (context, state) {
+        final itemId = int.tryParse(state.pathParameters['itemId'] ?? '') ?? 0;
+
+        return CreateBookingScreen(itemId: itemId);
+      },
+    ),
+    GoRoute(
+      path: '/customer/bookings',
+      name: 'customer-bookings',
+      builder: (context, state) => const CustomerBookingsScreen(),
+    ),
+    GoRoute(
+      path: '/customer/bookings/:id',
+      name: 'customer-booking-detail',
+      builder: (context, state) {
+        final id = int.tryParse(state.pathParameters['id'] ?? '') ?? 0;
+
+        return BookingDetailScreen(bookingId: id);
+      },
+    ),
+    GoRoute(
+      path: '/customer/payment-webview',
+      name: 'customer-payment-webview',
+      builder: (context, state) {
+        final args = state.extra as PaymentWebViewArgs?;
+
+        return PaymentWebViewScreen(
+          args:
+              args ??
+              const PaymentWebViewArgs(
+                redirectUrl: 'https://app.sandbox.midtrans.com/',
+                title: 'Pembayaran',
+              ),
+        );
+      },
+    ),
+    GoRoute(
+      path: '/customer/verifications/identity/:bookingId',
+      name: 'customer-identity-verification',
+      builder: (context, state) {
+        final bookingId =
+            int.tryParse(state.pathParameters['bookingId'] ?? '') ?? 0;
+
+        return IdentityVerificationScreen(bookingId: bookingId);
       },
     ),
     GoRoute(
