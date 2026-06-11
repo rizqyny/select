c99da363a4b2875e0eebf973355dbe7a8814b338
rzaldii <iqbal17.rizaldi@gmail.com>
2026-06-11 14:19:09 +0700

    fix item card

--- lib/features/customer/home/customer_home_screen.dart
+++ lib/features/customer/home/customer_home_screen.dart
@@ -226,7 +226,7 @@ class _CustomerHomeScreenState extends ConsumerState<CustomerHomeScreen> {
                               crossAxisCount: 2,
                               mainAxisSpacing: 14,
                               crossAxisSpacing: 14,
-                              childAspectRatio: 0.54,
+                              childAspectRatio: 0.57,
                             ),
                       ),
                     ),
